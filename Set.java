b9b9a2c71793b4fa08d0196e3e22ef7bc83c7f64
Drew E <56365730+squidlysquid81@users.noreply.github.com>
2019-11-13 09:36:19 -0500

    Update Set.java

--- Set.java
+++ Set.java
@@ -1,5 +1,16 @@
 package mastermind;
 //@author edwardsdw
 public class Set  {
-    
+    private Piece[] section;
+    public Set()  {
+        section = new Piece[4];
+        for (int count = 0; count < section.size(); count++)  {
+            section[count] = new Piece();
+        }
+    }
+    public Piece rSection(int place)  {
+        Piece output = new Piece();
+        if (place >= 0 && 
+            output.sColor();
+    }
 }
