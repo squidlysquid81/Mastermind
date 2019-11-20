d75c4073727b0d6bbba801125288226b67f1adc3
Drew E <56365730+squidlysquid81@users.noreply.github.com>
2019-11-20 13:49:40 -0500

    Update PlayerGuess.java

--- PlayerGuess.java
+++ PlayerGuess.java
@@ -2,5 +2,23 @@ package mastermind;
 import java.util.Scanner;
 //@author edwardsdw
 public class PlayerGuess extends TwoPlayer  {
-    
+    public PlayerGuess()  {
+        master = new Set();
+        guess = new Set();
+        sMaster();
+    }
+    public void sMaster()  {
+        guesses = 0;
+        for (int count = 0; count < Set.rPIECES(); count++)  {
+            master[count].randColor();
+            master[count].singProb(master[count].rColor());
+            for (int place = 0; place < Set.rPIECES(); place++)  {
+                if (place != count)
+                    master[place].zeroProb(master[count].rColor());
+            }
+        }
+    }
+    public int test()  {
+        return master.test(guess);
+    }
 }
