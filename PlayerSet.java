e43f548dee8d237aea8269daaffba5980845b38a
Drew E <56365730+squidlysquid81@users.noreply.github.com>
2019-11-20 14:10:37 -0500

    Update PlayerSet.java

--- PlayerSet.java
+++ PlayerSet.java
@@ -2,5 +2,18 @@ package mastermind;
 import java.util.Scanner;
 //@author edwardsdw
 public class PlayerSet extends TwoPlayer  {
-    
+    public PlayerSet()  {
+        master = new Set();
+        guess = new Set();
+        sMaster();
+    }
+    public void sGuess(int response)  {
+        update(response);
+        for (int count = 0; count < Set.rPIECES(); count++)  {
+            guess.section[count].randColor();
+        }
+    }
+    public void update(int response)  {
+        guess.update(response);
+    }
 }
