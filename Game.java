d87bbb60d58b2c8d0e7e29f9087ef12eda45d352
Drew E <56365730+squidlysquid81@users.noreply.github.com>
2019-11-20 13:14:20 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -2,6 +2,7 @@ package mastermind;
 //@author edwardsdw
 public interface Game  {
     public static int guesses = 0;
+    public static String[] responses = new String{"", "No colors are correct.", "", "Some colors are correct, but none are in the correct position.", "", "All colors are correct, but not all of them are in the correct position.", "", "Some colors are correct and in the right position.", "", "The set is correct."};
     public void sMaster(); //should set the master set
     public void sGuess(); //should set the guess set
     public int test(); //should test guess set against master set and return the response OR simply return the response
